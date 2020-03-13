0491857efee724651c05feb3eb622ed684fc120b
Ashley Harvey <aharveyokpolicy.org@Ashleys-MacBook-Pro.local>
2020-03-13 13:54:08 -0500

    Updated analysis

--- collections_small fees/01 query and clean .R
+++ collections_small fees/01 query and clean .R
@@ -25,84 +25,22 @@ collections <- collections %>%
 collections <- collections %>% 
   mutate(sheriff = str_detect(type, "SHERIFF"))
 
-collections <- collections %>% 
-  mutate(security = str_detect(type, "SHERIFF") & 
-         str_detect(type, "SEC"))
+collections$feetype <- case_when(collections$afis == TRUE ~ "AFIS",
+                                 collections$oids == TRUE ~ "OIDS",
+                                 collections$cleet == TRUE ~ "CLEET",
+                                 collections$casa == TRUE ~ "CASA",
+                                 collections$sheriff == TRUE ~ "SHERIFF",
+                                 TRUE ~ "OTHER")
 
-collections <- collections %>% 
-  mutate(jailfee = str_detect(type, "SHERIFF") & 
-           str_detect(type, "JAIL"))
+smallfees <- collections %>% 
+  filter(feetype != "OTHER")
 
-collections <- collections %>% 
-  mutate(sheriff_fees = str_detect(type, "SHERIFF") & 
-           str_detect(type, "FEES CIV"))
+total_sm <- smallfees %>% 
+  group_by(year, feetype) %>% 
+  summarize(fees = sum(fee, na.rm = TRUE, digits = 2))
 
-collections <- collections %>% 
-  mutate(drugtest = str_detect(type, "SHERIFF") & 
-           str_detect(type, "DRUG"))
 
-collections <- collections %>% 
-  mutate(erf = str_detect(type, "SHERIFF") & 
-           str_detect(type, "ENVI"))
-
-collections <- collections %>% 
-  filter(afis == TRUE| oids == TRUE| cleet == TRUE| casa == TRUE| sheriff == TRUE)
-
-total <- collections %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-#### summarize####
-oids <- collections %>% 
-  filter(oids == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-cleet <- collections %>% 
-  filter(cleet == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-afis <- collections %>% 
-  filter(afis == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-casa <- collections %>% 
-  filter(casa == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-sheriff_all <- collections %>% 
-  filter(sheriff == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-sheriff_security <- collections %>% 
-  filter(security == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-sheriff_jailfee <- collections %>% 
-  filter(jailfee == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-sheriff_fees <- collections %>% 
-  filter(sheriff_fees == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-sheriff_drugtest <- collections %>% 
-  filter(drugtest == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-sheriff_erf <- collections %>% 
-  filter(erf == TRUE) %>% 
-  group_by(year) %>% 
-  summarize(fees = sum(fee, na.rm = TRUE))
-
-#### ####
+####Checking data using aoc_collections ####
 connect_ojo()
 d <- dbReadTable(ojo_db, "aoc_collections")
 disconnect_ojo()
@@ -110,8 +48,8 @@ disconnect_ojo()
 d <- d %>% 
   mutate(casetype = (str_to_upper(casetype)))
 
-d <- d %>% 
-  filter(casetype == "FELONY" | casetype == "MISDEMEANOR")
 
 sum <- d %>% 
   summarize(fees = sum(coll_amt, na.rm = TRUE))
+
+sum(d$coll_amt)
